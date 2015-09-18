dcff56afc3b0e09ff9a42f488f74ee42aabdc0a3
Shankar Rao <raos11@rpi.edu>
2015-09-18 14:11:11 -0400

    Edited to main.js to include create bike calorie endpoint

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ var options = {
   // ** REQUIRED **
   access_token:  'aV1SI82xvTrQD9dI92wxOd4a72y0UpTSK8Bbjf0Tmx1C7LrLTyKlETfSyWCqpUSrKMOqX0Y-lWCFw6yPEhcRiFECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP'  // Access token for specific user,
 }
+var express = require('express')
+var io = require('socket.io').listen(8000);
+var fs = require('fs');
+var sys = require('sys');
+var app = express();
+    app.set("view options", {layout: false});
+    app.use(express.static(__dirname + '/views'));
 
+	app.set('port', (process.env.PORT || 5000));
 var up = require('jawbone-up')(options);
 //Calories burned = 5 x 68.2 kg x .5 hour = 170.5
 			//    ^^^^ //
@@ -24,12 +32,48 @@ function callback(err, body) {
     console.log("Calories burnt riding:"+ calories_riding)
     calories_driving = 2*68.2*time
     console.log("Calories burnt if driving:"+ calories_driving)
-
-
-
-
-
+		io.sockets.on('connection', function (socket) {
+		  socket.emit('for_client', { Title: data.items[0].title, Distance: data.items[0].details.km, Calories: calories_riding.toFixed(2), Time: time.toFixed(2)});
+		  socket.on('for_server', function(data) {
+	  	
+	  });
+	});
   }
 }
 
-up.workouts.get({}, callback)     
+function calculator(calories){
+
+	//Running
+	console.log(((calories/69)*262.5)/60 + " " +'minutes')
+	time = ((calories/69)*262.5)/60 
+	console.log(time/4.375 + " " + "km")
+
+	//Motor Cycle
+	console.log(((calories/170)*3600)/60 + " " +'minutes')
+	time = ((calories/170)*3600)/60 
+	console.log(time/4.375 + " " + "km")
+
+	return time
+
+}
+
+
+
+up.workouts.get({}, callback)    
+app.get('/', function(req, res){
+    res.render('/views/index.html', {title:'Hello'});
+});
+app.get('/#about', function(req, res){
+    res.render('/views/about.html');
+});
+app.get('/data', function(req, res){
+    res.send('Hello');  
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+// calculator(1000) 
